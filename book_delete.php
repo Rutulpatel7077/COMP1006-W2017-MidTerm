5ad2359a51bc1207c71efe840dbe0a7c432d2d14
Rutulpatel7077 <rutulpatel7077@gmail.com>
2017-02-22 16:27:00 -0500

    first commit

---
+++ book_delete.php
@@ -0,0 +1,19 @@
+<?php
+include_once('database.php');
+
+/*//////////////////////*/
+/* YOUR CODE GOES HERE */
+/*/////////////////////*/
+
+$bookID = $_GET["bookID"]; // assigns the bookID from the URL
+if($bookID != false) {
+    $query = "DELETE FROM books WHERE Id = :book_id";
+    $statement = $db->prepare($query);
+    $statement->bindValue(":book_id", $bookID);
+    $success = $statement->execute(); // execute the prepared query
+    $statement->closeCursor(); // close off database
+}
+
+header('Location: index.php');
+
+?>
---
+++ update_database.php
@@ -0,0 +1,44 @@
+<?php
+include_once('database.php');
+
+$isAddition = filter_input(INPUT_POST, "isAddition");
+$bookTitle = filter_input(INPUT_POST, "TitleTextField");
+$bookAuthor = filter_input(INPUT_POST, "AuthorTextField");
+$bookPrice = filter_input(INPUT_POST, "PriceTextField");
+$bookGenre = filter_input(INPUT_POST, "GenreTextField");
+
+// check if user is Adding a New Book
+if($isAddition == "1") {
+
+/*//////////////////////*/
+/* FIX THIS MYSQL QUERY */
+/*//////////////////////*/
+
+$query="INSERT INTO books (Title,Author,Price,Genre) VALUES (:book_title, :book_author,:book_price,:book_genre)";
+
+$statement = $db->prepare($query); // encapsulate the sql statement
+}
+// else if user is Updating an Existing Book
+else {
+$bookID = filter_input(INPUT_POST, "IDTextField");
+
+/*//////////////////////*/
+/* FIX THIS MYSQL QUERY */
+/*//////////////////////*/
+
+$query = "UPDATE books SET Name = :book_name, Cost = :book_cost WHERE Id = :book_id ";
+$statement = $db->prepare($query); // encapsulate the sql statement
+$statement->bindValue(':book_id', $bookID);
+
+}
+
+$statement->bindValue(':book_title', $bookTitle);
+$statement->bindValue(':book_author', $bookAuthor);
+$statement->bindValue(':book_price', $bookPrice);
+$statement->bindValue(':book_genre', $bookGenre);
+$statement->execute(); // run on the db server
+$statement->closeCursor(); // close the connection
+
+// redirect to index page
+header('Location: index.php');
+?>
